1bc216067324006c4bb6f7093c46ca1d783a247e
Dimitris Zeus <63305437+dimitriosg@users.noreply.github.com>
2026-04-02 11:20:07 +0300

    feat: add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { LangProvider } from './LangContext.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LangProvider>
       <App />
+      <SpeedInsights />
     </LangProvider>
   </StrictMode>,
 )
