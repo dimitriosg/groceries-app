ca8a35416d6d0a4b1f95e4bf3337fe90585ba9b7
Dimitris Zeus <63305437+dimitriosg@users.noreply.github.com>
2026-04-02 16:55:38 +0300

    feat: add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { LangProvider } from './LangContext.jsx'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <LangProvider>
       <App />
       <SpeedInsights />
+      <Analytics />
     </LangProvider>
   </StrictMode>,
 )
